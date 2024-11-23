ed04c64e0af676c3f0ded67c6835532b1cb788fe
Ana Molina <ana713w@gmail.com>
2024-11-23 20:07:03 +0100

    done

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,50 @@
-function CountryDetails() {}
+import { Link, useParams } from "react-router-dom";
+import countries from "../assets/countries.json"
 
-export default CountryDetails;
+function CountryDetails() {
+    const { countryId } = useParams();
+
+    const country = countries.find((c) => c.alpha3Code === countryId);
+
+    return (
+      <div className="container">
+        <p style={{ fontSize: "24px", fontWeight: "bold" }}>Country Details</p>
+  
+        <h1>{country.name.common}</h1>
+  
+        <table className="table">
+          <thead></thead>
+          <tbody>
+            <tr>
+              <td style={{ width: "30%" }}>Capital</td>
+              <td>{country.capital}</td>
+            </tr>
+            <tr>
+              <td>Area</td>
+              <td>
+                {country.area} km
+                <sup>2</sup>
+              </td>
+            </tr>
+            <tr>
+              <td>Borders</td>
+              <td>
+                <ul>
+                    {country.borders.map((b) => {
+                        const borderCountry = countries.find((c) => c.alpha3Code === b);
+                         return (
+                            <li key={b}>
+                                <Link to={`/${b}`}>{borderCountry.name.common}</Link>
+                            </li>
+                    );
+                    })}
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  
+  export default CountryDetails;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,34 @@
-function HomePage() {}
+import { Link } from "react-router-dom";
+import countries from "../assets/countries.json"
 
-export default HomePage;
+function HomePage() {
+    return (
+      <div
+        className="container"
+        style={{ maxHeight: "90vh", overflow: "scroll" }}
+      >
+        <h1 style={{ fontSize: "24px" }}>
+          WikiCountries: Your Guide to the World
+        </h1>
+  
+        <div className="list-group">
+            {countries.map((country) => (
+                <Link 
+                    key={country.alpha2Code}
+                    className="list-group-item list-group-item-action"
+                    to={`/${country.alpha3Code}`} 
+                >
+                    <img 
+                        src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLocaleLowerCase()}.png`}
+                        alt={`${country.name.common} flag`}
+                        style={{width: "20px", height:"16px", marginRight: '10px'}}
+                    />
+                    {country.name.common}
+                </Link>
+            ))}
+        </div>
+      </div>
+    );
+  }
+  
+  export default HomePage;
